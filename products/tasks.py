e31a9acc5e4a72841e3aa587a063cea728761730
VasilevskiyRA <vasilevskiyra@byterg.ru>
2020-09-28 12:58:33 +0300

    add check extra value; set time_limit for check_rosstat;

--- products/tasks.py
+++ products/tasks.py
@@ -2,9 +2,9 @@
 from __future__ import absolute_import, unicode_literals
 import time
 
-from celery.schedules import crontab
 from django.db.models import Q
 from django.conf import settings
+from celery.schedules import crontab
 import requests
 
 from bsma.celery import app
@@ -19,15 +19,22 @@ def setup_periodic_tasks(sender, **kwargs):
     )
 
 
-@app.task(ignore_result=True)
+@app.task(time_limit=60*60*24, ignore_result=True)
 def check_rosstat():
-    articles = Article.objects.filter(Q(product_id=1), (Q(extra__isnull=True) | ~Q(extra__devices=[])))
+    articles = Article.objects.filter(Q(product_id=1) & (Q(extra__isnull=True) | Q(extra__devices=[]))).order_by(
+        '-created_at')
     for i, article in enumerate(articles, 1):
         imei = article.imei.replace('-', '')
         url = f'https://nottheapi.rosstat.cloud.rt.ru:8443/apiman-gateway/Byterg/getDeviceActivationStatus/1.0/{imei}'
         r = requests.get(url, auth=(settings.ROS_USER, settings.ROS_PASSWORD), params={'apikey': settings.ROS_API_KYE})
-        time.sleep(1)
+        time.sleep(0.5)
+
+        if r.headers['content-type'] != 'application/json':
+            continue
 
         if 'devices' in r.json():
-            article.extra = r.json()
+            if articles.extra is None:
+                article.extra = r.json()
+            if 'devices' in articles.extra:
+                articles.extra['devices'] = r.json()['devices']
             article.save()
